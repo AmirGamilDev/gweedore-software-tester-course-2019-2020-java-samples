e5f9d2bdf751ddbcbf7d2d50cc594148a953be6c
Amir Gamil <amir.gamil.dev@gmail.com>
2019-08-15 09:24:06 +0100

    Add pre and post increment example

---
+++ src/ie/nln/softwaretester/basics/PreAndPost.java
@@ -0,0 +1,25 @@
+package ie.nln.softwaretester.basics;
+
+public class PreAndPost {
+
+	public static void main(String[] args) {
+		int num1 = 0;
+		
+		System.out.println(num1);
+		
+		num1++;
+		
+		System.out.println(num1);
+		
+		++num1;
+		
+		System.out.println(num1);
+		
+		
+		System.out.println(num1++);
+		
+		System.out.println(num1);
+		
+		System.out.println(++num1);
+	}
+}
